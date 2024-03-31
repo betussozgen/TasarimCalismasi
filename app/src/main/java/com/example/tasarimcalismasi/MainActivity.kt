45d69d9cca07fcc08fe97eb8c02b1a16242ee84e
betussozgen <fatmabetulozgen@hotmail.com>
2024-03-31 21:43:16 +0300

    TasarimCalismasi

---
+++ app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tasarimcalismasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Yazılım kısmında R harfi res klasörünü temsil eder.
+        //Xml kısmında @ işrati res klasörünü temsil eder.
+        //Left - Start - Leading
+        //Right - End- Trailing
+    }
+}
